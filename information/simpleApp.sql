d70bddd43c6f8c1a7b0c902d672adad246a5750a
root <supavut_chardchedsak@bitbucket.org>
2016-09-13 19:27:05 +0700

    Update sql

--- information/simpleApp.sql
+++ information/simpleApp.sql
@@ -53,17 +53,6 @@ CREATE TABLE IF NOT EXISTS `Infomation` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
---
--- Dumping data for table `Infomation`
---
-
-INSERT INTO `Infomation` (`id`, `firstname`, `lastname`, `address`, `gender`, `tel`, `email`, `created_at`, `updated_at`) VALUES
-(1, 'ศุภวุฒิ', 'ชาติเชิดศักดิ์', 'testtest', 0, '', 'supavut.chardchedsak@gmail.com', '2016-09-13 17:31:25', '2016-09-13 17:31:25'),
-(2, 'admin', 'admin', 'testtest', 1, '0000000000', 'thelastsubper@gmail.com', '2016-09-13 17:33:32', '2016-09-13 17:33:32'),
-(3, 'test', 'test', 'testtest', 0, '0874102553', 'the_lastsubper@hotmail.com', '2016-09-13 19:11:18', '2016-09-13 19:11:18'),
-(4, 'test2', 'test2', 'test2', 0, '0874102553', 'the_lastsubper@hotmail.com', '2016-09-13 19:12:26', '2016-09-13 19:12:26'),
-(5, 'test3', 'test', 'test2', 0, '0874102553', 'the_lastsubper@hotmail.com', '2016-09-13 19:13:14', '2016-09-13 19:13:14'),
-(6, 'test344', '4444444', 'testtest', 0, '0874102553', 'allansmith@yahoo.com', '2016-09-13 19:15:15', '2016-09-13 19:15:15');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
